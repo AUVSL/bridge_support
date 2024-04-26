e0e4d3ba4507e48f280f34c9a5455f3d67741324
Weihao <weihaoc4@illinois.edu>
2024-04-26 15:35:14 -0500

    bridge launch script path modified

--- bridge_support_ros2/param_bridge.sh
+++ bridge_support_ros2/param_bridge.sh
@@ -1,5 +1,5 @@
 source /opt/ros/humble/setup.bash
-source ~/obstacle_det_ws/install/setup.bash
+source ~/colcon_ws/install/setup.bash
 source ~/ros-humble-ros1-bridge/install/local_setup.bash
 
 ros2 run ros1_bridge parameter_bridge
